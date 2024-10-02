5cf047ca77f966a0d534fd414108b0a6598f1996
Gabriel Martinez <gaabi26092005@gmail.com>
2024-10-02 20:27:33 -0300

    Nueva configuarion en el archivo webpack.config y archivo de configuracion de babelrc listo

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const path = require('path');
-
 module.exports = {
     entry: './src/app/index.js',
     output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'src', 'public')  // Debe apuntar a una ruta válida en tu proyecto
+        path: __dirname + '/src/public',
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude:/node_modules/
+            }
+        ]
     }
-}
+};
