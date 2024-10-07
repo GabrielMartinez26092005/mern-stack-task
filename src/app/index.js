98d0464b7c44d5ea3643fb363a78465dc2a672f8
Gabriel Martinez <gaabi26092005@gmail.com>
2024-10-06 21:28:19 -0300

    Comenzando con el frontend.

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,5 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { render } from 'react-dom'
+import App from './App'
 
-class App extends Component{
-    render(){
-        return(
-            <h1>Hello World</h1>
-        )
-    }
-}
 render(<App></App>, document.getElementById('app'))
