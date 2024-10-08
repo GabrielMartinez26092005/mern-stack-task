fcf382c1b443044193939a0ffca594aae5e02803
Gabriel Martinez <gaabi26092005@gmail.com>
2024-10-08 00:04:16 -0300

    Siguiendo con el frontend.

--- src/app/App.js
+++ src/app/App.js
@@ -7,6 +7,7 @@ class App extends Component{
         this.state = {
             title: '',
             description: '',
+            tasks: [],
         }
         this.handleChange = this.handleChange.bind(this)
         this.addTask = this.addTask.bind(this)
@@ -21,9 +22,29 @@ class App extends Component{
                 'Content-Type': 'application/json' // Corrige el typo aquí
             }
         })
-        .then(res => console.log(res))
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            M.toast({html: 'Task Saved'})
+            this.setState({title: '', description: ''})
+            this.fetchTasks()
+        })
         .catch(err => console.error(err))
         e.preventDefault(); 
+    }
+
+    componentDidMount(){
+        this.fetchTasks()
+    }
+
+    fetchTasks(){
+        fetch('/api/tasks')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({tasks: data})
+                console.log(this.state.tasks)
+            })
+            .catch(err => console.error(err))
     }
 
     handleChange(e){
@@ -31,7 +52,11 @@ class App extends Component{
         this.setState({
             [name]: value
         })
-     }
+    }
+
+    // deleteTask(id){
+
+    // }
 
     render(){
         return(
@@ -43,7 +68,6 @@ class App extends Component{
                         </a>
                     </div>
                 </nav>
-
                 <div className='container'>
                     <div className='row'>
                         <div className='col s5'>
@@ -52,12 +76,12 @@ class App extends Component{
                                     <form onSubmit={this.addTask}>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input name='title' onChange={this.handleChange} type="text" placeholder='Task Title'/>
+                                                <input name='title' onChange={this.handleChange} type="text" placeholder='Task Title' value={this.state.title}/>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <textarea name='description' onChange={this.handleChange} placeholder='Task Description' className='materialize-textarea'></textarea>
+                                                <textarea name='description' onChange={this.handleChange} placeholder='Task Description' className='materialize-textarea' value={this.state.description}></textarea>
                                             </div>
                                         </div>
                                         <button type='submit' className='btn light-blue darken-4'>
@@ -66,6 +90,37 @@ class App extends Component{
                                     </form>
                                 </div>
                             </div>
+                        </div>
+                        <div className='col s7'>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map((task) => {
+                                            return (
+                                                <tr key={task.id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                    <td>
+                                                        <button className='btn light-blue darken-4'>
+                                                            <i className='material-icons'>delete</i>
+                                                        </button>
+                                                        <button className='btn light-blue darken-4'>
+                                                            <i className='material-icons'>edit</i>
+                                                        </button>
+                                                        {/* <button onClick={() => this.deleteTask(task.id)}>Delete</button> */}
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
